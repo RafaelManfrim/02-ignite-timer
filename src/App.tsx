c76622f894afb217153ba7252c2a4ddbde9a1c18
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-08 23:03:30 -0300

    Configurando Tema Styled-Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <div>Hello world</div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
