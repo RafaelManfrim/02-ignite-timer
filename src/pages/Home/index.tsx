fc43b51026c41c0118fbd504ac57fd208495d430
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-15 20:46:31 -0300

    Mudando título da página dinâmicamente

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -85,6 +85,12 @@ export function Home() {
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
 
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `Ignite Timer - ${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
+
   const isSubmitDisabled = !watch('task')
 
   return (
