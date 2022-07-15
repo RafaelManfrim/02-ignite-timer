67fc35f294c6fbbc166ef1cb8b1dbd79a74cb46a
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-15 00:26:17 -0300

    reset do formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,7 +24,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, watch, handleSubmit } = useForm<NewCycleFormData>({
+  const { register, watch, handleSubmit, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -34,6 +34,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   const isSubmitDisabled = !watch('task')
