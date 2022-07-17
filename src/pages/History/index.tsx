7c94f192556f59cc6b1c12775d891e82f49b9a19
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-17 11:19:55 -0300

    Formatando data de início

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,6 @@
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { useCycles } from '../../hooks/useCycles'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
@@ -22,7 +25,12 @@ export function History() {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
-                <td>{cycle.startDate.toLocaleDateString()}</td>
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
                 <td>
                   {cycle.finishedDate ? (
                     <Status statusColor="green">Concluído</Status>
